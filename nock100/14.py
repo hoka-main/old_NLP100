ce17af3f969acc67a91610525966a208e58dd092
ohiroki-sp <64342567+ohiroki-sp@users.noreply.github.com>
2020-06-18 23:54:55 +0900

    コマンドライン引数を用いて14．pyを実行できました

--- nock100/14.py
+++ nock100/14.py
@@ -5,8 +5,13 @@ args = sys.argv
 print(args)
 
 print('第1引数:' + args[1])
-intN = args[1]
-with open('popular_names.txt', mode='w') as names:
-    for line in names:
-        cols = line.split('\t')
+intN = int(args[1])
+with open('cols.txt', mode='r') as names:
+    for line, N in zip(names, range(intN)):
+        if N < 0:
+            break
+        else:
+            print(line)
+            N -= 1
+
 
