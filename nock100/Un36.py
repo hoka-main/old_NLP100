39bde143d06234c5e13b763012cd0b0a24f02e9c
hoka-sp <64342567+hoka-sp@users.noreply.github.com>
2020-09-10 19:53:44 +0900

    ３６思考中

--- nock100/Un36.py
+++ nock100/Un36.py
@@ -1,7 +1,10 @@
 import sys
+import re
 from collections import Counter
 import Un35
 import nock30
+import numpy
+from matplotlib import pyplot
 
 
 def main():
@@ -9,11 +12,18 @@ def main():
     args.append('neko.txt.mecab')
     phrase_list = [nock30.parse_mecab(phrase)
                    for phrase in nock30.make_phrase_list(args[1])]
+    counter = 9
 
+    # kigou = ["。", "、", "「", "」"]
+    # regex = ""
+    # re.sub(regex, surface)
+    # pattern = re.compile('[「」。、]+')
     for index, dict_box in enumerate(Un35.word_pop_frequency(phrase_list)):
         print(dict_box)
-        if index == 10:
+        if index == counter:
             break
+    pyplot.plot(dict_box)
+    pyplot.show()
 
 
 if __name__ == '__main__':
