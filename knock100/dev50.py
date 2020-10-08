136cf67444fa9140f0b8032b41dc7ff126e1db58
hoka-sp <64342567+hoka-sp@users.noreply.github.com>
2020-10-09 00:47:49 +0900

    dev50.51backup

--- knock100/dev50.py
+++ knock100/dev50.py
@@ -9,21 +9,24 @@ from sklearn.model_selection import train_test_split
 '''
 def read_csv(csv):
     with open(csv, newline='') as csv_file:
-        spam_reader = csv.reader(csv_file, delimiter=' ', quotechar='|')
+        spam_reader = csv.reader(csv_file, 
+                                delimiter=' ', quotechar='|')
         for row in spam_reader:
             print(', '.join(row))
 '''
 
 
 def file_writer(data, file_name):
-    with open(file_name, mode='w') as writing_file:
+    with open(file_name, mode='w')\
+            as writing_file:
         for line in data:
             for line2 in line:
                 writing_file.write(line2)
                 writing_file.write('\t')
             writing_file.write('\n')
     # 受け取ったデータをtabで区切りながらデータを入力する関数。
-    # データ一行ごとに改行し、受け取ったデータをごちゃまぜにならないようにしている。
+    # データ一行ごとに改行し、受け取ったデータを
+    # ごちゃまぜにならないようにしている。
 
 
 def write_data(remaining_count, file_name_list, target_news_list):
@@ -72,7 +75,9 @@ def sub2():
     args = sys.argv
     args.append('newsCorpora.csv')
     args.append('2pageSessions.scv')
-    target_publisher = ['Reuters', 'Huffington Post', 'Businessweek', 'Contactmusic.com', 'Daily Mail']
+    target_publisher = ['Reuters', 'Huffington Post',
+                        'Businessweek', 'Contactmusic.com',
+                        'Daily Mail']
     file_name_list = ['train.txt', 'valid.txt', 'test.txt']
 
     target_news_list = []
@@ -102,17 +107,82 @@ def sub2():
         # 抽出したデータをランダムに並び替える
         print(len(target_news_list))    # 確認用
         percent80 = len(target_news_list) * 0.8
-        remaining_count.append(math.floor(percent80))
-        remaining_count.append(int((len(target_news_list) - remaining_count[0]) / 2))
-        remaining_count.append(int(len(target_news_list) - (remaining_count[0] + remaining_count[1]) - 3))
+        remaining_count.append(
+            math.floor(percent80))
+        remaining_count.append(
+            int((len(target_news_list) - remaining_count[0]) / 2))
+        remaining_count.append(
+            int(len(target_news_list) -
+                (remaining_count[0] + remaining_count[1]) - 3))
         print(remaining_count)  # 確認用
-        # 抽出したデータを[約80%][残りの20%　/2][残り]でファイルに書き込む回数を決める
+        # 抽出したデータを[約80%][残りの20%　/2][残り]で
+        # ファイルに書き込む回数を決める
     write_data(remaining_count, file_name_list, target_news_list)
 
 
+def test():
+    # データの読込
+    df = pd.read_csv('ch06/newsCorpora_re.csv', header=None, sep='\t',
+                     names=['ID', 'TITLE', 'URL', 'PUBLISHER', 'CATEGORY', 'STORY', 'HOSTNAME', 'TIMESTAMP'])
+
+    # データの抽出
+    df = df.loc[
+        df['PUBLISHER'].isin(['Reuters', 'Huffington Post', 'Businessweek', 'Contactmusic.com', 'Daily Mail']), [
+            'TITLE', 'CATEGORY']]
+
+    # データの分割
+    train, valid_test = train_test_split(df, test_size=0.2, shuffle=True, random_state=123, stratify=df['CATEGORY'])
+    valid, test = train_test_split(valid_test, test_size=0.5, shuffle=True, random_state=123,
+                                   stratify=valid_test['CATEGORY'])
+
+    # データの保存
+    train.to_csv('ch06/train.txt', sep='\t', index=False)
+    valid.to_csv('ch06/valid.txt', sep='\t', index=False)
+    test.to_csv('ch06/test.txt', sep='\t', index=False)
+
+    # 事例数の確認
+    print('【学習データ】')
+    print(train['CATEGORY'].value_counts())
+    print('【検証データ】')
+    print(valid['CATEGORY'].value_counts())
+    print('【評価データ】')
+    print(test['CATEGORY'].value_counts())
+
+
 def main():
-    read_data = pd.read_cv('')
+    args = sys.argv
+    args.append('ch06/newsCorpora_re.csv')
+    read_data = pd.read_csv(
+        args[1], sep='\t', names=[
+            'ID', 'TITLE', 'URL', 'PUBLISHER', 'CATEGORY',
+            'STORY', 'HOSTNAME', 'TIMESTAMP'])
+
+    read_data = read_data.loc[
+        read_data['PUBLISHER'].isin(
+            ['Reuters', 'Huffington Post',
+             'Business_week', 'Contact_music.com', 'Daily_Mail']
+        ), ['TITLE', 'CATEGORY']
+    ]
+
+    train, valid_test = train_test_split(
+        read_data, test_size=0.2, shuffle=True, random_state=123,
+        stratify=read_data['CATEGORY']
+    )
+
+    valid, test = train_test_split(
+        valid_test, test_size=0.5, shuffle=True, random_state=123,
+        stratify=valid_test['CATEGORY']
+    )
+
+    train.to_csv('ch06/train.txt', sep='\t', index=False)
+    valid.to_csv('ch06/valid.txt', sep='\t', index=False)
+    test.to_csv('ch06/test.txt', sep='\t', index=False)
+
+    print(train['CATEGORY'].value_counts())
+    print(valid['CATEGORY'].value_counts())
+    print(test['CATEGORY'].value_counts())
 
 
 if __name__ == '__main__':
+    # test()
     main()
--- knock100/dev51.py
+++ knock100/dev51.py
@@ -1,7 +1,29 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
+import sys
+
+
+def test_def(args):
+    return_file = []
+    del args[0]
+    for file in args:
+        text = pd.read_csv(file, sep='\t', names=['TITLE', 'CATEGORY'])
+        title = text.TITLE
+        return_file.append(title)
+    return return_file
 
 
 def main():
+    args = sys.argv
+    args.append('ch06/train.txt')
+    args.append('ch06/valid.txt')
+    args.append('ch06/test.txt')
+    with open(args[1], mode='r') as train, \
+            open(args[2], mode='r') as valid, \
+            open(args[3], mode='r') as test:
+        pass
+    test = test_def(args)
+    test.to_
 
 
 if __name__ == '__main__':
