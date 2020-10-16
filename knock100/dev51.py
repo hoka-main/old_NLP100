ef98ac95f9c6f43915749f93e1ffca023e9a27c2
hoka-sp <64342567+hoka-sp@users.noreply.github.com>
2020-10-16 16:56:41 +0900

    knock51-52.backup

--- knock100/dev51.py
+++ knock100/dev51.py
@@ -5,13 +5,24 @@ import re
 import string
 
 
-def make_default_title_list(title_file_list):
+def make_default_title_list():
+    file_pass = [
+        'ch06/train.feature.txt',
+        'ch06/valid.feature.txt',
+        'ch06/test.feature.txt'
+        ]
+
     train = []
     valid = []
     test = []
-
-    for i, items in enumerate(title_file_list):
-        items.replace(',', '').replace('.', '')
+    read_file = []
+    with open(file_pass[0]) as train_id,\
+            open(file_pass[1]) as valid_id,\
+            open(file_pass[2]) as test_id:
+        read_file.append(train_id.read().replace(',', '').replace('.', ''))
+        read_file.append(valid_id.read().replace(',', '').replace('.', ''))
+        read_file.append(test_id.read().replace(',', '').replace('.', ''))
+    for i, items in enumerate(read_file):
         if i == 0:
             for line in items:
                 train.append(line)
@@ -25,46 +36,49 @@ def make_default_title_list(title_file_list):
     return title_list_of_news_list
 
 
-def read_def(args):
+def read_dataframe_title():
+    file_pass = [
+        'ch06/train.txt',
+        'ch06/valid.txt',
+        'ch06/test.txt'
+        ]
     return_file = []
-    del args[0]
-    for file in args:
+    for file in file_pass:
         text = pd.read_csv(file, sep='\t', names=['TITLE', 'CATEGORY'])
         title = text.TITLE
         return_file.append(title)
     return return_file
 
 
-def dataframe_of_tfidf(sample):
+def dataframe_of_tfidf(train, valid, test):
     vec_tfidf = TfidfVectorizer()
-    x = vec_tfidf.fit_transform(sample)
+    dataframe = pd.concat([train, valid, test])  # データを結合している
+    vec_tfidf.fit(dataframe)
+    return_frame = [vec_tfidf.transform(train), vec_tfidf.transform(valid), vec_tfidf.transform(test)]
     print('Vocabulary size: {}'.format(len(vec_tfidf.vocabulary_)))
     # print('Vocabulary content: {}'.format(vec_tfidf.vocabulary_))
-
-    dataframe = pd.DataFrame(x.toarray(), columns=vec_tfidf.get_feature_names())
+    # print(return_frame)
+    dataframe1 = pd.DataFrame(return_frame[0].toarray(), columns=vec_tfidf.get_feature_names())
+    dataframe2 = pd.DataFrame(return_frame[1].toarray(), columns=vec_tfidf.get_feature_names())
+    dataframe3 = pd.DataFrame(return_frame[2].toarray(), columns=vec_tfidf.get_feature_names())
+    dataframe = [dataframe1, dataframe2, dataframe3]
     return dataframe
 
 
 def main():
-    args = sys.argv
-    args.append('ch06/train.txt')
-    args.append('ch06/valid.txt')
-    args.append('ch06/test.txt')
-
+    title_frame = read_dataframe_title()
+    data = dataframe_of_tfidf(title_frame[0], title_frame[1], title_frame[2])  # データフレーム形式でタイトルを抽出する
+    # title_list = make_default_title_list()  # 出現単語を登録し、三つのデータに分割したリストを出力
     save_file_pass_list = [
         'ch06/train.feature.txt',
         'ch06/valid.feature.txt',
         'ch06/test.feature.txt'
-    ]
-    data = read_def(args)  # タイトルを抽出する
-
-    title_list = make_default_title_list(data) # データを分ける
-
+        ]
     dataframe_list = []
-    for file_pass, news in zip(save_file_pass_list, title_list):
-        dataframe = dataframe_of_tfidf(news)
-        dataframe.to_csv(file_pass)
-        dataframe_list.append(dataframe)
+
+    for file_pass, news in zip(save_file_pass_list, data):
+        print(news)
+        news.to_csv(file_pass)
     # データフレームにtfidfデータを当て込みファイルに保存する
     '''
     tfidf_vec = TfidfVectorizer(token_pattern=u'(?u)\\b\\w+\\b')
@@ -75,7 +89,6 @@ def main():
     print(tfidf_train)
 #    print(tfidf_vec.get_feature_names())
     '''
-    return dataframe_list
 
 
 if __name__ == '__main__':
--- knock100/dev52.py
+++ knock100/dev52.py
@@ -1,43 +1,41 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-import sys
-import dev51
 import pandas as pd
 
 
-def read_def(args):
-    return_file = []
-    del args[0]
-    for file in args:
-        text = pd.read_csv(file, sep='\t', names=['TITLE', 'CATEGORY'])
-        title = text.CATEGORY
-        return_file.append(title)
-    return return_file
-
-
-def main():
-    args = sys.argv
-    feature_txt_pass = [
-        'ch06/train.feature.txt',
-        'ch06/valid.feature.txt',
-        'ch06/test.feature.txt'
-    ]
+def read_def():
     default_txt_pass = [
         'ch06/train.txt',
         'ch06/valid.txt',
         'ch06/test.txt'
     ]
+    return_file = []
+    for file in default_txt_pass:
+        text = pd.read_csv(file, sep='\t', names=['TITLE', 'CATEGORY'])
+        category = text.CATEGORY
+        return_file.append(category)
+    return return_file
+
+
+def main():
+    feature_txt_pass = [
+        'ch06/train.feature.txt',
+        'ch06/valid.feature.txt',
+        'ch06/test.feature.txt'
+    ]
+
     x_id = []
     for file in feature_txt_pass:
-        with open(file[0]) as file_x_id:
+        with open(file) as file_x_id:
             x_id.append(file_x_id.read())
+    train_x = x_id[2]
+    dataframe_list = read_def()
+    train = dataframe_list[2]
+    # valid = dataframe_list[1]
+    # test = dataframe_list[2]
 
-    dataframe_list = read_def(default_txt_pass)
-    train = dataframe_list[0]
-    valid = dataframe_list[1]
-    test = dataframe_list[2]
     lg = LogisticRegression(random_state=123, max_iter=10000)
-    lg.fit(train_x, train['CATEGORY'])
+    lg.fit(train_x, train)
 
 
 if __name__ == '__main__':
