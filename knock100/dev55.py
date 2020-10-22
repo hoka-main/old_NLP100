0dba51a270d12939fcd19d6fa71b155395f84096
hoka-sp <64342567+hoka-sp@users.noreply.github.com>
2020-10-22 17:34:28 +0900

    knock55.backup

--- knock100/dev55.py
+++ knock100/dev55.py
@@ -8,8 +8,7 @@ def main():
     x_train = Log[0]
     y_train = Log[1]
     train_pred = dev53.pred(x_train, y_train)
-    breakpoint()
-    train_cm = confusion_matrix(y_train[1], train_pred[1])
+    train_cm = confusion_matrix(y_train, train_pred)
     print(train_cm)
 
 
